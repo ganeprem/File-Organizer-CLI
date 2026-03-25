eecff63f72d1f652bbe0ab4d711be2d320bde6a2
Ganesh V P <221942416+ganeprem@users.noreply.github.com>
2026-03-25 15:37:52 +0530

    Implement minimal Undo functionality

--- organizer.py
+++ organizer.py
@@ -18,18 +18,22 @@ def organize_folder(folder_path):
     if not os.path.isdir(folder_path):
         print("Invalid directory")
         return
-
+    count = {}
     for item in os.listdir(folder_path):
         item_path = os.path.join(folder_path, item)
 
         if os.path.isfile(item_path):
             category = get_category(item)
+            count[category] = count.get(category, 0) + 1
             target_dir = os.path.join(folder_path, category)
 
             os.makedirs(target_dir, exist_ok=True)
             shutil.move(item_path, os.path.join(target_dir, item))
 
     print("Organization complete.")
+    for category, c in count.items():
+        print(f"{category}: {c} files")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Organize files by type")
